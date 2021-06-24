392deada810fd47398150450e288884d5e47eda0
wandering-sage <shivam4503@gmail.com>
2021-06-25 01:49:48 +0530

    day 1

---
+++ 01- Sort Colors/bestSol.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Two pointers Meathod also known as Dutch National Flag Algorithm
+
+void sortColors(vector<int> &nums)
+{
+  int l = 0;
+  int cur = 0;
+  int h = nums.size() - 1;
+  while (cur <= h)
+  {
+    if (nums[cur] == 0)
+    {
+      if (l != cur)
+        swap(nums[l], nums[cur]);
+      l++;
+      cur++;
+    }
+    else if (nums[cur] == 1)
+      cur++;
+    else
+    {
+      swap(nums[h], nums[cur]);
+      h--;
+    }
+  }
+}
