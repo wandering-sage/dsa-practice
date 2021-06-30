947423ae5c50a802d1247909b0fef19fc50caf95
wandering-sage <shivam4503@gmail.com>
2021-07-01 02:18:13 +0530

    day 7

---
+++ 22- Longest Subarray Zero Sum/test.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rng(int lim)
+{
+  uniform_int_distribution<int> uid(0, lim - 1);
+  return uid(rang);
+}
+
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+//=======================
+
+int subarraySum(vector<int> &nums, int k)
+{
+  unordered_map<int, int> map;
+  int sum = 0;
+  int max = 0;
+  map[0] = -1;
+  for (int i = 0; i < nums.size(); i++)
+  {
+    sum += nums[i];
+    if (map.find(sum) == map.end())
+    {
+      map[sum] = i;
+    }
+    if (map.find(sum - k) != map.end())
+    {
+      int cur = i - map[sum - k];
+      if (max < cur)
+        max = cur;
+    }
+  }
+  return max;
+}
+
+void solve()
+{
+  vector<int> a = {1, 2, 1, 2, 1};
+  cout << subarraySum(a, 3);
+  cout << "Hello";
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+  int t = 1;
+  // cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+
+  return 0;
+}
