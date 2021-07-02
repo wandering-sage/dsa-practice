0cf12e023104644fdb9fcb9bc757618f44a67766
wandering-sage <shivam4503@gmail.com>
2021-07-02 15:01:51 +0530

    Updated

--- 08- Pascal's Triangle/sol2.cpp
+++ 08- Pascal's Triangle/sol2.cpp
@@ -29,22 +29,3 @@ vector<long long> nthRowOfPascalTriangle(int n)
   }
   return ans[ans.size() - 1];
 }
-
-// vector<long long> nthRowOfPascalTriangle(int n)
-// {
-//   vector<long long> ans;
-//   long long s;
-//   ans.push_back(1);
-//   for (int j = 1; j <= (n - 1) / 2; j++)
-//   {
-//     long long x = (ans[j - 1] * (n - j) / j);
-//     x %= 1000000007;
-//     ans.push_back(x);
-//   }
-//   int m = ans.size() - 1;
-//   for (int j = n % 2; j <= (n - 1) / 2; j++)
-//   {
-//     ans.push_back(ans[m - j]);
-//   }
-//   return ans;
-// }
